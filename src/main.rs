384f729af40e82e51371089623cec4ce88902496
Tom Martin <tom@tpgmartin.com>
2016-05-24 00:31:42 +0100

    Create basic server

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#[macro_use] extern crate nickel;
+
+use nickel::{Nickel, MediaType};
+
+fn main() {
+
+    let mut server = Nickel::new();
+    
+    server.utilize(router! {
+        get "/foo" => |_request, mut response| {
+            response.set(MediaType::Json);
+            r#"{ "foo": "bar" }"#
+        }
+    });
+    
+    server.listen("127.0.0.1:6767");
+
+}
